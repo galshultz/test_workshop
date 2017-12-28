7a48bd9dafc0bacfa3bdbb1d8291fd9ade27c371
yoni <yoni@adgorithms.com>
2017-12-28 18:17:24 +0200

    initial commit

---
+++ database/database.py
@@ -0,0 +1,34 @@
+import os.path
+import csv
+from random import random
+from uuid import uuid4
+
+class Database(object):
+    def __init__(self, coin):
+        self.file_name = 'database/coins/{}.csv'.format(coin)
+        assert os.path.isfile(self.file_name), "Unknown Coin"
+
+    def get_all_transactions(self):
+        with open(self.file_name) as f:
+            reader = csv.DictReader(f)
+            return [
+                {'source': t['source'] or None, 'destination': t['destination'],'amount': float(t['amount'])}
+                for t in reader
+            ]
+
+    def add_transaction(self, source, destination, amount):
+        if random() > 0:
+            raise DatabaseError("DB Conn Fail - err {}".format(uuid4()))
+
+        transactions = self.get_all_transactions()
+        transactions.append({'source': source, 'destination': destination,'amount': amount})
+        with open(self.file_name, 'w') as f:
+            writer = csv.DictWriter(f, fieldnames=('source', 'destination', 'amount'))
+            writer.writeheader()
+            writer.writerows(transactions)
+
+        return True
+
+
+class DatabaseError(Exception):
+    pass
---
+++ transactor.py
@@ -0,0 +1,85 @@
+from database import Database, DatabaseError
+import logging
+import sys
+
+# setup logging configuration
+logging.basicConfig(
+    stream=sys.stdout, level=logging.DEBUG,
+    format='[%(asctime)s][%(name)s][%(levelname)s][%(funcName)s]:%(message)s'
+)
+
+
+class Coin(object):
+    """Supported Coin Types"""
+    CAT = 'cat'
+    DOG = 'dog'
+
+
+class Transactor(object):
+    """Manage transaction validation and book keeping of coins"""
+    _ADD_RETRY_ATTEMPTS = 2
+
+    def __init__(self, coin_type):
+        """instantiate a new Transactor instance for a specific coin type"""
+
+        self.coin_type = coin_type
+        self.db = Database(coin_type)
+        self.logger = logging.getLogger(__name__)
+        self.logger.info("Starting a new instance for coin '{}'".format(self.coin_type))
+
+    def add_transaction(self, source, destination, amount):
+        """
+        Validate and persist a transaction between source and destination addresses
+        :param source: the giving party
+        :param destination: the receiving party
+        :param amount: amount of coins for the transaction
+        """
+        amount = float(amount)
+        if not self._source_credit_is_valid(source, amount):
+            raise InvalidTransaction("source {} credit cannot support the required transaction".format(source))
+
+        self._add_transaction(source, destination, amount)
+
+    def get_previous_transactions(self):
+        """returns a list of all previous transactions as dicts in the form {source: str, destination: str, amount: float}"""
+        return self.db.get_all_transactions()
+
+    @staticmethod
+    def get_address_balance(address, transactions):
+        in_amount = sum(t['amount'] for t in transactions if t['destination'] == address) or 0.0
+        out_amount = sum(t['amount'] for t in transactions if t['source'] == address) or 0.0
+        return in_amount - out_amount
+
+    def _source_credit_is_valid(self, source_address, amount):
+        """Validates that the source address has the necessary funds to perform the required transaction"""
+
+        transactions = self.get_previous_transactions()
+        balance = self.get_address_balance(source_address, transactions)
+        self.logger.debug("Source {} balance: {}{}s".format(source_address, balance, self.coin_type))
+
+        return amount <= balance
+
+    def _add_transaction(self, source, destination, amount, retry_attempts=_ADD_RETRY_ATTEMPTS):
+        """persists a valid transaction in the database"""
+
+        try:
+            self.db.add_transaction(source, destination, amount)
+
+        except DatabaseError:
+            if retry_attempts > 0:
+                self.logger.warning("Failed to save transaction to DB - Retrying", exc_info=True)
+                return self._add_transaction(source, destination, amount, retry_attempts=retry_attempts-1)
+
+            self.logger.exception("Failed to save transaction to database - Retry attempts exceeded")
+            raise TransactorGeneralError("Failed to save transaction to DB")
+
+        else:
+            self.logger.info("Successfully added transaction")
+
+
+class TransactorGeneralError(Exception):
+    pass
+
+
+class InvalidTransaction(TransactorGeneralError):
+    pass
