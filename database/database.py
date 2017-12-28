4063209eccfcd81c9b4f741bf5127e48d2b62218
yoni <yoni@adgorithms.com>
2017-12-28 18:48:08 +0200

    add randomness to database errors

--- database/database.py
+++ database/database.py
@@ -17,7 +17,7 @@ class Database(object):
             ]
 
     def add_transaction(self, source, destination, amount):
-        if random() > 0:
+        if random() > 0.9:
             raise DatabaseError("DB Conn Fail - err {}".format(uuid4()))
 
         transactions = self.get_all_transactions()
