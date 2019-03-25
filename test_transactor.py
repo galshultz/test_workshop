fcafed8ff5b190e033ed063b2419ca76c4f7eee4
galshultz <galshultz88@gmail.com>
2019-03-25 09:25:07 +0200

    Update test_transactor.py

--- test_transactor.py
+++ test_transactor.py
@@ -2,50 +2,86 @@ import pytest, mock
 import transactor
 from database import DatabaseError
 
-@pytest.fixture
-def mock_db_transaction_source_present():
-    return [{'source': 'a',
-             'amount': 100.0,
-             'destination': 'b'}]
 
-@pytest.fixture
-def mock_db_transaction_source_not_present():
-    return [{'source': None,
-             'amount': 100.0,
-             'destination': 'b'}]
+class TestTransactorInit(object):
+    def test_transactor_init(self):
+        coin_init = transactor.Transactor('cat')
+        assert coin_init
 
-
-def test_transactor_init():
-    coin_init = transactor.Transactor('cat')
-    assert coin_init
-
-def test_transactor_init_fail():
-    with pytest.raises(AssertionError):
-        transactor.Transactor('moshe')
-
-
-def test__calc_address_balance_source_present(mock_db_transaction_source_present):
-    with mock.patch('database.Database.get_all_transactions') as mock_database:
-        mock_database.return_value = mock_db_transaction_source_present
-        transactions = mock_database.return_value
-        transactor_test = transactor.Transactor('test')
-        assert -100 == transactor_test._calc_address_balance('a',transactions)
-
-def test__calc_address_balance_source_none(mock_db_transaction_source_not_present):
-    with mock.patch('database.Database.get_all_transactions') as mock_database:
-        mock_database.return_value = mock_db_transaction_source_not_present
-        transactions = mock_database.return_value
-        transactor_test = transactor.Transactor('test')
+    def test_transactor_init_fail(self):
         with pytest.raises(AssertionError):
+            transactor.Transactor('moshe')
+
+class TestPartyBalance(object):
+    def test__calc_address_balance_source_present(self, mock_db_transaction_source_present):
+        with mock.patch('database.Database.get_all_transactions') as mock_database:
+            mock_database.return_value = mock_db_transaction_source_present
+            transactions = mock_database.return_value
+            transactor_test = transactor.Transactor('test')
             assert -100 == transactor_test._calc_address_balance('a',transactions)
 
-def test_add_transaction():
-    with mock.patch('database.Database.add_transaction') as mock_database:
-        mock_database.side_effect = [DatabaseError(), True]
-        transactor_test = transactor.Transactor('cat')
+    def test__calc_address_balance_source_none(self, mock_db_transaction_source_not_present):
+        with mock.patch('database.Database.get_all_transactions') as mock_database:
+            mock_database.return_value = mock_db_transaction_source_not_present
+            transactions = mock_database.return_value
+            transactor_test = transactor.Transactor('test')
+            with pytest.raises(AssertionError):
+                assert -100 == transactor_test._calc_address_balance('a',transactions)
 
-        source = 'bea73734-603b-43cf-aa65-ad23d5c3f3e3'
-        destination = 'dee4e97f-0a1c-4ec4-8b41-66e9dd5ec9d1'
-        amount = 1
+class TestAddTransactionFails(object):
+    def test_add_transaction_one_fail(self,mock_db_get_all_transactions):
+        with mock.patch('database.Database.add_transaction') as mock_add_transaction:
+            with mock.patch('database.Database.get_all_transactions') as mock_get_all_trnasactions:
+                mock_get_all_trnasactions.return_value = mock_db_get_all_transactions
+                mock_add_transaction.side_effect = [DatabaseError, None]
+                transactor_test = transactor.Transactor('test')
 
-        assert transactor_test.add_transaction(source,destination,amount)
+                source = 'a'
+                destination = 'b'
+                amount = 1
+
+                assert transactor_test.add_transaction(source,destination,amount) == None
+
+    def test_add_transaction_all_fail(self,mock_db_get_all_transactions):
+        with mock.patch('database.Database.add_transaction') as mock_add_transaction:
+            with mock.patch('database.Database.get_all_transactions') as mock_get_all_trnasactions:
+                mock_get_all_trnasactions.return_value = mock_db_get_all_transactions
+                mock_add_transaction.side_effect = DatabaseError
+                transactor_test = transactor.Transactor('test')
+
+                source = 'a'
+                destination = 'b'
+                amount = 1
+
+                with pytest.raises(transactor.TransactorGeneralError):
+                        transactor_test.add_transaction(source,destination,amount)
+
+    def test_add_transaction_all_fail(self,mock_db_get_all_transactions):
+        with mock.patch('database.Database.add_transaction') as mock_add_transaction:
+            with mock.patch('database.Database.get_all_transactions') as mock_get_all_trnasactions:
+                mock_get_all_trnasactions.return_value = mock_db_get_all_transactions
+                mock_add_transaction.side_effect = DatabaseError
+                transactor_test = transactor.Transactor('test')
+
+                source = 'a'
+                destination = 'b'
+                amount = 1
+
+                with pytest.raises(transactor.TransactorGeneralError):
+                        transactor_test.add_transaction(source,destination,amount)
+                assert mock_add_transaction.call_count == 3
+
+class TestAddTransactionSuccess(object):
+    def test_add_transaction_success(self,mock_db_get_all_transactions):
+        with mock.patch('database.Database.add_transaction') as mock_add_transaction:
+            with mock.patch('database.Database.get_all_transactions') as mock_get_all_trnasactions:
+                mock_get_all_trnasactions.return_value = mock_db_get_all_transactions
+                transactor_test = transactor.Transactor('test')
+                mock_add_transaction.return_value = True
+
+                source = 'a'
+                destination = 'b'
+                amount = 1
+
+                transactor_test.add_transaction(source, destination, amount)
+                mock_add_transaction.assert_called_once_with(source, destination, amount)
